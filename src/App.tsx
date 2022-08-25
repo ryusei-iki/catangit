35387d3f35aae202ad58b7d9e209618359bb8576
ryusei-iki <0516.iki@gmail.com>
2022-08-25 14:01:59 +0900

    mapNum aut generate

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const shuffle = ([...array]) => {
 const sigen_num=[3,4,3,4,4,1]//Brick,Lumber,Ore,Grain,Wool,Nothing
 const sigen: number[] = [];
 const sigenCoordinate=new Array(19)
-const mapNum=[3,4,5,4,5]
+const mapNum=[3,4,5,4,3]
 // alert(sigenCoordinate)
 const imageSize=100
 const haba=[imageSize,imageSize/2,0,imageSize/2,imageSize]
@@ -36,15 +36,19 @@ for (let i=0;i<mapNum.length;i++){
 }
 
 const numArray=[2,3,3,4,4,5,5,6,6,8,8,9,9,10,10,11,11,12]
-const numArrayRandom=shuffle(numArray)
-console.log('numArray')
+// const numArrayRandom=shuffle(numArray)
+// console.log('numArray')
+// alert('numArrayRandom1')
+// alert(numArrayRandom)
+
 // alert('numarray')
-const numArrayRandomMap=new Array(5)
+// const numArrayRandomMap=new Array(5)
 for(let i=0 ;i<6;i++){
   for (let j=0;j<sigen_num[i];j++){
     sigen.push(i)
   }
 }
+// alert(sigen)
 // const sigenRandom=shuffle(sigen)
 // count=0
 // for(let i=0;i<mapNum.length;i++){
@@ -62,72 +66,149 @@ for(let i=0 ;i<6;i++){
 //   }
 // }
 const sigenRandom=shuffle(sigen)
-count=0
-for(let i=0;i<mapNum.length;i++){
-  for(let j=0;j<mapNum[i];j++){
-    if(j==0){
-      numArrayRandomMap[i]=new Array(mapNum[i])
-    }
-    if(numArrayRandom[count]==5){
-      //pass
-    }
-    else{
-      numArrayRandomMap[i][j]=numArrayRandom[count]
-    }
-    count=count+1
-  }
-}
+// alert(sigenRandom)
+// count=0
+// let check=0
+// for(let i=0;i<mapNum.length;i++){
+//   for(let j=0;j<mapNum[i];j++){
+//     if(j==0){
+//       numArrayRandomMap[i]=new Array(mapNum[i])
+//     }
+//     if(sigenRandom[count]==5){
+//       numArrayRandomMap[i][j]=7
+//       check=1
+//     }
+//     else{
+//       numArrayRandomMap[i][j]=numArrayRandom[count-check]
+//     }
+//     count=count+1
+//   }
+// }
+// alert(numArrayRandomMap)
+
 export const App=()=> {
 const [dame,setDame]=useState(0)
+let dame_num=1
+  // console.log('start')
+  const func= ()=>{
+    alert('start')
+    // let numArrayRandomMap=new Array(5)
+    let numArrayRandom=shuffle(numArray)
 
-  console.log('start')
-  useEffect(()=>{
-    // alert('effect')
-    for(let i=0;i<Math.floor(mapNum.length/2);i++){
-    count=0
-    console.log(i)
-    for(let j=0;j<mapNum[i];j++){
-      console.log(j)
-      if(numArrayRandomMap[i][j]==6||numArrayRandomMap[i][j]==8){
-        // console.log('ddd')
-          if(j==mapNum[i]-1){
-            if(numArrayRandomMap[i+1][j]==6||numArrayRandomMap[i+1][j]==8){//下の左を見る
-              console.log(11)
-              setDame((prev)=>prev+1)
-            }
-            if(numArrayRandomMap[i+1][j+1]==6||numArrayRandomMap[i+1][j+1]==8){//下の右を見る
-              console.log(22)
-              setDame((prev)=>prev+1)
+    while(dame_num!=0){
+      let numArrayRandomMap=new Array(5)
+      let check=0
+      dame_num=0
+      count=0
+      numArrayRandom=shuffle(numArray)
 
-            }
+      for(let i=0;i<mapNum.length;i++){ //数字番号のランダム生成
+        for(let j=0;j<mapNum[i];j++){
+          if(j==0){
+            numArrayRandomMap[i]=new Array(mapNum[i])
+          }
+          if(sigenRandom[count]==5){
+            numArrayRandomMap[i][j]=7
+            check=1
           }
           else{
-            if(numArrayRandomMap[i][j+1]==6||numArrayRandomMap[i][j+1]==8){ //同列の右を見る
-              console.log(33)
-              setDame((prev)=>prev+1)
-
-
-            }
-            if(numArrayRandomMap[i+1][j]==6||numArrayRandomMap[i+1][j]==8){//下の左を見る
-              console.log(44)
-              setDame((prev)=>prev+1)
-
-            }
-            if(numArrayRandomMap[i+1][j+1]==6||numArrayRandomMap[i+1][j+1]==8){//下の右を見る
-              console.log(55)
-              setDame((prev)=>prev+1)
-
-            }
+            numArrayRandomMap[i][j]=numArrayRandom[count-check]
           }
+          count=count+1
         }
       }
-    }},[])
+    for(let i=0;i<mapNum.length;i++){//数字番号のランダム生成で赤が隣り合っているかの判定
+    count=0
+    // console.log(i)
+    for(let j=0;j<mapNum[i];j++){
+      console.log(numArrayRandomMap[i][j])
+      if(numArrayRandomMap[i][j]==6||numArrayRandomMap[i][j]==8){//6か8のとき，隣り合わせチェックが必要なとき
+        if(i<Math.floor(mapNum.length/2)){//半分より上のとき
+          if(j==mapNum[i]-1){//一番右を見ているとき
+            //
+          }
+          else{//一番右以外を見ているとき，隣に数字が有るとき
+              if(numArrayRandomMap[i][j+1]==6||numArrayRandomMap[i][j+1]==8){ //同列の右を見る
+                console.log(111)
+                // setDame((prev)=>prev+1)
+                dame_num=dame_num+1
+              }
+          }
+          if(numArrayRandomMap[i+1][j]==6||numArrayRandomMap[i+1][j]==8){//下の左を見る
+            console.log(222)
+            // setDame((prev)=>prev+1)
+            dame_num=dame_num+1
+
+          }
+          if(numArrayRandomMap[i+1][j+1]==6||numArrayRandomMap[i+1][j+1]==8){//下の右を見る
+            console.log(333)
+            // setDame((prev)=>prev+1)
+            dame_num=dame_num+1
+
+          }
+        }
+        else if(i==mapNum.length-1){//一番下のとき
+          if(numArrayRandomMap[i][j+1]==6||numArrayRandomMap[i][j+1]==8){ //同列の右を見る
+            console.log(111)
+            // setDame((prev)=>prev+1)
+            dame_num=dame_num+1
+
+          }
+        }
+        else{//半分より下のとき
+          if(j==mapNum[i]-1){//一番右を見ているとき
+            if(numArrayRandomMap[i+1][j-1]==6||numArrayRandomMap[i+1][j-1]==8){//下の左を見る
+              console.log(222)
+              // setDame((prev)=>prev+1)
+              dame_num=dame_num+1
+            }
+          }
+          else if(j==0){
+            if(numArrayRandomMap[i+1][j]==6||numArrayRandomMap[i+1][j]==8){//下の右を見る
+              console.log(333)
+              // setDame((prev)=>prev+1)
+              dame_num=dame_num+1
+            }
+            if(numArrayRandomMap[i][j+1]==6||numArrayRandomMap[i][j+1]==8){ //同列の右を見る
+              console.log(111)
+                  // setDame((prev)=>prev+1)
+                  dame_num=dame_num+1
+                }
+          }
+          else{
+          if(numArrayRandomMap[i][j+1]==6||numArrayRandomMap[i][j+1]==8){ //同列の右を見る
+            console.log(111)
+                // setDame((prev)=>prev+1)
+                dame_num=dame_num+1
+              }
+              if(numArrayRandomMap[i+1][j-1]==6||numArrayRandomMap[i+1][j-1]==8){//下の左を見る
+                console.log(222)
+                // setDame((prev)=>prev+1)
+                dame_num=dame_num+1
+              }
+              if(numArrayRandomMap[i+1][j]==6||numArrayRandomMap[i+1][j]==8){//下の右を見る
+                console.log(333)
+                // setDame((prev)=>prev+1)
+                dame_num=dame_num+1
+              }
+            }
+        }
+
+        }
+      }
+    }
+    // alert(dame_num)
+    // dame_num=0
+  }
+  return numArrayRandom
+  }
+  let numArrayRandom=func()
 
   let tobasu=0
   return (
     <>
-    {/* <div>{dame}</div> */}
-    <div>{numArrayRandomMap}</div>
+    <div>{dame}</div>
+    <div>{numArrayRandom}</div>
         <svg width={imageSize*5} height={imageSize*5}>
   {sigenRandom.map((sigen,index)=>{return <><image xlinkHref={svgList[sigen]} x={sigenCoordinate[index][0]}y={sigenCoordinate[index][1]}/>
 
